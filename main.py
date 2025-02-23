936376fa1cbe38968e3f17634a0d01b586d50a22
Diego Berdeja Suárez <dberdejas@gmail.com>
2025-02-22 21:30:55 -0600

    Periodic commit.

--- main.py
+++ main.py
@@ -5,12 +5,34 @@ from PySide6 import QtGui as qtg
 
 from classes import Ingredient, Recipe, Recetario, MainWindow, populator
 
-
+# We define a list of possible properties.
+PROPERTIES = {
+    'Calcium': True,
+    'Calories': True,
+    'Carbohydrates': True,
+    'Cholesterol': True,
+    'Fibre': True,
+    'Iron': True,
+    'Mono-saturated Fat': True,
+    'Name': True,
+    'Poly-saturated Fat': True,
+    'Potassium': True,
+    'Protein': True,
+    'Saturated Fat': True,
+    'Sodium': True,
+    'Sugar': True,
+    'Total Fat': True,
+    'Vitamin A': True,
+    'Vitamin B12': True,
+    'Vitamin B6': True,
+    'Vitamin C': True,
+    'Vitamin E': True,
+    'Vitamin K': True
+}
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv) # In QT we must always have an app object. There can only be one. This launches the Python Launcher.
-
     window = MainWindow() # Note window is not a QT Window object, but rather our own inherited object.
-
-    window.save_to_file(window.recipes) # We save to file before quitting.
+    window.save_to_file(window.recipes, window.properties) # We save to file before quitting.
     sys.exit(app.exec()) # The .exec starts the app loop and waits until exit is called.
+
